8cac5f1cc5a0e772090c782aacb455cf4d997c9f
ascotte <scottealexandre@gmail.com>
2024-09-04 10:30:21 +0200

    [init] first commit

--- anchor/target/types/gem_quest.ts
+++ anchor/target/types/gem_quest.ts
@@ -5,108 +5,162 @@
  * IDL can be found at `target/idl/gem_quest.json`.
  */
 export type GemQuest = {
-  address: 'ApLERHGGnmjWrTqoDFUVKotBZ7ZcDELcjQHyGNXG5APB';
-  metadata: {
-    name: 'gemQuest';
-    version: '0.1.0';
-    spec: '0.1.0';
-    description: 'Created with Anchor';
-  };
-  instructions: [
+  "address": "ApLERHGGnmjWrTqoDFUVKotBZ7ZcDELcjQHyGNXG5APB",
+  "metadata": {
+    "name": "gemQuest",
+    "version": "0.1.0",
+    "spec": "0.1.0",
+    "description": "Created with Anchor"
+  },
+  "instructions": [
     {
-      name: 'close';
-      discriminator: [98, 165, 201, 177, 108, 65, 206, 96];
-      accounts: [
+      "name": "close",
+      "discriminator": [
+        98,
+        165,
+        201,
+        177,
+        108,
+        65,
+        206,
+        96
+      ],
+      "accounts": [
         {
-          name: 'payer';
-          writable: true;
-          signer: true;
+          "name": "payer",
+          "writable": true,
+          "signer": true
         },
         {
-          name: 'gemQuest';
-          writable: true;
+          "name": "gemQuest",
+          "writable": true
         }
-      ];
-      args: [];
+      ],
+      "args": []
     },
     {
-      name: 'decrement';
-      discriminator: [106, 227, 168, 59, 248, 27, 150, 101];
-      accounts: [
+      "name": "decrement",
+      "discriminator": [
+        106,
+        227,
+        168,
+        59,
+        248,
+        27,
+        150,
+        101
+      ],
+      "accounts": [
         {
-          name: 'gemQuest';
-          writable: true;
+          "name": "gemQuest",
+          "writable": true
         }
-      ];
-      args: [];
+      ],
+      "args": []
     },
     {
-      name: 'increment';
-      discriminator: [11, 18, 104, 9, 104, 174, 59, 33];
-      accounts: [
+      "name": "increment",
+      "discriminator": [
+        11,
+        18,
+        104,
+        9,
+        104,
+        174,
+        59,
+        33
+      ],
+      "accounts": [
         {
-          name: 'gemQuest';
-          writable: true;
+          "name": "gemQuest",
+          "writable": true
         }
-      ];
-      args: [];
+      ],
+      "args": []
     },
     {
-      name: 'initialize';
-      discriminator: [175, 175, 109, 31, 13, 152, 155, 237];
-      accounts: [
+      "name": "initialize",
+      "discriminator": [
+        175,
+        175,
+        109,
+        31,
+        13,
+        152,
+        155,
+        237
+      ],
+      "accounts": [
         {
-          name: 'payer';
-          writable: true;
-          signer: true;
+          "name": "payer",
+          "writable": true,
+          "signer": true
         },
         {
-          name: 'gemQuest';
-          writable: true;
-          signer: true;
+          "name": "gemQuest",
+          "writable": true,
+          "signer": true
         },
         {
-          name: 'systemProgram';
-          address: '11111111111111111111111111111111';
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
         }
-      ];
-      args: [];
+      ],
+      "args": []
     },
     {
-      name: 'set';
-      discriminator: [198, 51, 53, 241, 116, 29, 126, 194];
-      accounts: [
+      "name": "set",
+      "discriminator": [
+        198,
+        51,
+        53,
+        241,
+        116,
+        29,
+        126,
+        194
+      ],
+      "accounts": [
         {
-          name: 'gemQuest';
-          writable: true;
+          "name": "gemQuest",
+          "writable": true
         }
-      ];
-      args: [
+      ],
+      "args": [
         {
-          name: 'value';
-          type: 'u8';
+          "name": "value",
+          "type": "u8"
         }
-      ];
+      ]
     }
-  ];
-  accounts: [
+  ],
+  "accounts": [
     {
-      name: 'gemQuest';
-      discriminator: [135, 64, 223, 168, 233, 143, 162, 215];
+      "name": "gemQuest",
+      "discriminator": [
+        243,
+        109,
+        250,
+        1,
+        183,
+        182,
+        59,
+        147
+      ]
     }
-  ];
-  types: [
+  ],
+  "types": [
     {
-      name: 'gemQuest';
-      type: {
-        kind: 'struct';
-        fields: [
+      "name": "gemQuest",
+      "type": {
+        "kind": "struct",
+        "fields": [
           {
-            name: 'count';
-            type: 'u8';
+            "name": "count",
+            "type": "u8"
           }
-        ];
-      };
+        ]
+      }
     }
-  ];
+  ]
 };
